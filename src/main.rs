47fe176d9a6d1993528fbe83600463efb95ef780
Night_Hunter <samuelhuntnz@gmail.com>
2024-12-27 01:28:34 +1300

    remove everything

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
-mod window_capture_sub;
 
 use iced::{widget::image, Application, Command, Element, Settings};
 
@@ -13,57 +12,4 @@ fn main() -> iced::Result {
         .finish();
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-
-    Recon::run(Settings::default())
-}
-
-struct Recon {
-    image: iced::widget::image::Handle,
-}
-
-#[derive(Debug, Clone)]
-pub enum Message {
-    ButtonPressed,
-    NewFrame(iced::widget::image::Handle),
-}
-
-impl Application for Recon {
-    type Executor = iced::executor::Default;
-    type Message = Message;
-    type Theme = iced::Theme;
-    type Flags = ();
-
-    fn new(_flags: ()) -> (Recon, Command<Message>) {
-        (Recon {
-            image: iced::widget::image::Handle::from_pixels(1, 1, vec![0, 0, 0, 0]),
-        }, Command::none())
-    }
-
-    fn theme(&self) -> Self::Theme {
-        iced::Theme::Dark
-    }
-
-    fn title(&self) -> String {
-        String::from("My Iced App")
-    }
-
-    fn subscription(&self) -> iced::Subscription<Self::Message> {
-        window_capture_sub::create_window_capture_sub()
-    }
-
-    fn update(&mut self, message: Message) -> Command<Message> {
-        match message {
-            Message::ButtonPressed => {
-                println!("Button pressed!");
-            }
-            Message::NewFrame(image) => {
-                self.image = image;
-            }
-        }
-        Command::none()
-    }
-
-    fn view(&self) -> Element<Message> {
-        image(self.image.clone()).into()
-    }
 }
